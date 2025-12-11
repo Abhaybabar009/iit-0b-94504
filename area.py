702a2ffefb579acc1ead01174755d50dc4dc878c
Abhaybabar009 <abhaybabar781@gmail.com>
2025-12-11 09:42:12 +0530

    first commit

---
+++ area.py
@@ -0,0 +1,8 @@
+def Rectangle(l,b):
+    return l*b
+
+def Square(l):
+    return l*l
+
+def Circle(r):
+    return 3.14*r*r
---
+++ main.py
@@ -0,0 +1,6 @@
+import area as a
+l=10
+b=5
+r=7
+print(a.Circle(r))
+print(a.Square(l))
